b9f0fd14d98e139f5da863e2b3a528b24c99cf6a
eduardo.comin <eduardo.comin@unoesc.edu.br>
2014-10-25 09:53:37 -0200

    Nova modificação.

--- ProjetoGit/src/projetogit/ProjetoGit.java
+++ ProjetoGit/src/projetogit/ProjetoGit.java
@@ -16,7 +16,7 @@ public class ProjetoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Teste hehehe");
+        System.out.println("Teste Modificado");
     }
     
 }
